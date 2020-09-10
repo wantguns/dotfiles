3f1d8b43809211823a615d5e702b558a3ab26ede
Gunwant Jain <therealgunwant@gmail.com>
2020-09-10 08:29:49 +0000

    install.sh: preinstall docker, croc and rclone
    Signed-off-by: Gunwant Jain <therealgunwant@gmail.com>

--- install.sh
+++ install.sh
@@ -14,6 +14,20 @@ curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
 rm -rf ripgrep_11.0.2_amd64.deb
 
+# install docker and friends
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce docker-compose -y
+
+# install croc
+curl https://getcroc.schollz.com | bash
+
+# install rclone
+curl https://rclone.org/install.sh | sudo bash
+
 # XDG
 sudo bash -c 'cat >> /etc/profile << EOF
 
