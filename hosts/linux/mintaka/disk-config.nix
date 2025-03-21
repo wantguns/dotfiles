11ea2ab1f40f395323afc43d9005a11b48894352
Gunwant Jain <mail@wantguns.dev>
2025-03-21 23:48:43 +0530

    mintaka: disks: enable legacy mountpoint option to prevent zfs race
    ZFS and systemd race to mount some datastores like /home on boot, and
    because of that, the system ends up in a recovery state.
    
    see: https://github.com/nix-community/disko/issues/581
    
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- hosts/linux/mintaka/disk-config.nix
+++ hosts/linux/mintaka/disk-config.nix
@@ -49,22 +49,27 @@
           "root" = {
             type = "zfs_fs";
             mountpoint = "/";
+            options.mountpoint = "legacy";
           };
           "root/nix" = {
             type = "zfs_fs";
             mountpoint = "/nix";
+            options.mountpoint = "legacy";
           };
           "root/var" = {
             type = "zfs_fs";
             mountpoint = "/var";
+            options.mountpoint = "legacy";
           };
           "root/var/log" = {
             type = "zfs_fs";
             mountpoint = "/var/log";
+            options.mountpoint = "legacy";
           };
           "root/home" = {
             type = "zfs_fs";
             mountpoint = "/home";
+            options.mountpoint = "legacy";
           };
           "root/swap" = {
             type = "zfs_volume";
