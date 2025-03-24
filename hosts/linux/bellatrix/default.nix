ba4e4f054ae34405d1bdcf61b32f79c56616f7ad
Gunwant Jain <mail@wantguns.dev>
2025-03-24 17:31:26 +0000

    secrets: add bellatrix pub age key
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- hosts/linux/bellatrix/default.nix
+++ hosts/linux/bellatrix/default.nix
@@ -26,6 +26,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcueIcj4fgzD6cUUGqituoupjexNNF1Hjrr+dyJ+gvA gunwant.jain@C02GH2V9MD6M.local"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMm/9RWSPMxyeMHglw6cyZBgtuke+7l5wc9dXmQkiii"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIIA5UkkDnsE/Td4aa0N+2pZ05xAHvPE8SMVk5zlHhxA"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN+U9xlrIVyWY7DzhMO6Tf+JN04a9nzcdMc7nLOnWqq wantguns@mintaka"
     ];
   };
 
