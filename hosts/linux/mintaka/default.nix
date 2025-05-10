de0b76b536700f866beda285a1869751b06c1fea
Gunwant Jain <mail@wantguns.dev>
2025-05-10 14:57:21 +0000

    mintaka: enable docker
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- hosts/linux/mintaka/default.nix
+++ hosts/linux/mintaka/default.nix
@@ -11,6 +11,8 @@
     };
   };
 
+  virtualisation.docker.enable = true;
+
   # see: https://github.com/nix-community/disko/issues/581#issuecomment-2024231487
   fileSystems."/home".options = [ "noauto" ];
 
