f083d8c5a0b3a28839404e473e8f17ea18b4a155
Gunwant Jain <mail@wantguns.dev>
2025-10-25 22:35:14 +0000

    alnitak: setup k3s, bump flake
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- hosts/linux/alnitak/k3s.nix
+++ hosts/linux/alnitak/k3s.nix
@@ -1,23 +1,36 @@
 { config, pkgs, lib, inputs, ... }:
+let
+  gatewayApiCrds = pkgs.fetchurl {
+    url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml";
+    sha256 = "sha256-akAp5mFEbWSt2GagDs3EDBQhm2h3erYUxc2qwK20gfE=";
+  };
+in
 {
-   # adding some extra modules for better cilium support
-  boot.kernelModules = [ "br_netfilter" "ip_conntrack" "ip_vs" "ip_vs_rr" "ip_vs_wrr" "ip_vs_sh" "overlay" ];  
-
   networking.firewall = {
     allowedTCPPorts = [
-      6443 # apiserver
-      2379 # ha etcd clients
-      2380 # ha etcd peers
-      8472 # cilium
-      10250 # metrics-server
+      80
+      443
+      6443  # kube-apiserver
+      4244  # required by hubble-peer
+      10250 # metrics server
     ];
-    checkReversePath = "loose";
+    allowedUDPPorts = [
+      8472 # cilium vxlan
+    ];
+    checkReversePath = false;
     trustedInterfaces = [
       "cilium_host"
       "cilium_net"
       "cilium_vxlan"
       "lxc*"
+      "wg_orion"
     ];
+
+    extraInputRules = ''
+      ip saddr 10.69.0.0/16 accept
+      ip saddr 10.42.0.0/16 accept
+      ip saddr 10.43.0.0/16 accept
+    '';
   };
 
   services.k3s = {
@@ -30,7 +43,95 @@
       "--disable-network-policy"
       "--disable servicelb"
       "--disable traefik"
+      "--node-ip=10.69.0.1"
+      "--node-external-ip=78.46.83.190"
       "--debug"
     ];
+    
+    manifests = {
+      gateway-api = {
+        source = gatewayApiCrds;
+      };
+    };
+    
+    autoDeployCharts = {
+      cilium = {
+        name = "cilium";
+        repo = "https://helm.cilium.io";
+        version = "1.18.2";
+        hash = "sha256-ObYqcvCJLdFlSL0I7pfV2y6XX3wfVxVVeKFEGG4imS8=";
+        targetNamespace = "kube-system";
+        createNamespace = false;
+        extraFieldDefinitions.spec.bootstrap = true;
+        values = {
+          operator = {
+            replicas = 1;
+          };
+
+          k8sServiceHost = "10.69.0.1";
+          k8sServicePort = "6443";
+          routingMode = "tunnel";
+          tunnelPort = 8472;
+          autoDirectNodeRoutes = false;
+          kubeProxyReplacement = true;
+
+          ipam = {
+            mode = "kubernetes";
+          };
+
+          gatewayAPI = {
+            enabled = true;
+            hostNetwork = {
+              enabled = true;
+            };
+          };
+
+          hubble = {
+            enabled = true;
+            relay = {
+              enabled = true;
+            };
+            ui = {
+              enabled = true;
+            };
+          };
+
+          envoy = {
+            securityContext = {
+              capabilities = {
+                keepCapNetBindService = true;
+                envoy = [
+                  "NET_ADMIN"
+                  "SYS_ADMIN"
+                  "NET_BIND_SERVICE"
+                ];
+              };
+            };
+          };
+        };
+      };
+
+      argocd = {
+        name = "argo-cd";
+        repo = "https://argoproj.github.io/argo-helm";
+        version = "9.0.5";
+        hash = "sha256-Vm42yonj7KPyV1derzzfAFjzBEiSXUmkFROO4CPJk5w=";
+        targetNamespace = "argocd";
+        createNamespace = true;
+        extraDeploy = [
+          ./manifests/argo-resources.yaml
+        ];
+        values = {
+          configs = {
+            params = {
+              "server.insecure" = true;
+            };
+            cm = {
+              "timeout.reconciliation" = "60s";
+            };
+          };
+        };
+      };
+    };
   };
 }
