9a25d76c8e64885cd61488c92a8cebc48247c7e7
Gunwant Jain <mail@wantguns.dev>
2026-06-09 22:18:56 +0530

    mpv: set font provider for darwin, customise subtitles
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- modules/home/mpv/default.nix
+++ modules/home/mpv/default.nix
@@ -12,11 +12,27 @@ in
 lib.mkIf cfg.mpv {
   programs.mpv = {
     enable = true;
-    scripts = with pkgs.mpvScripts; [
-      twitch-chat
-      sponsorblock-minimal
-      quality-menu
+    extraMakeWrapperArgs = lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+      "--add-flags"
+      "--osd-font-provider=fontconfig"
     ];
+    scripts = [
+      pkgs.mpvScripts.uosc
+      pkgs.mpvScripts.twitch-chat
+      pkgs.mpvScripts.sponsorblock-minimal
+      pkgs.mpvScripts.quality-menu
+    ];
+    config = {
+      sub-font = "Noto Sans Bold";
+      sub-font-size = 30;
+      sub-color = "0.89/0.69/0.02/0.9";
+      sub-back-color = "0.0/0.0/0.0/0.20";
+      sub-border-size = 0;
+      sub-border-style = "background-box";
+      sub-shadow-offset = 5;
+      sub-blur = 0.01;
+    };
   };
+  home.packages = [ pkgs.noto-fonts ];
   programs.streamlink.enable = true;
 }
