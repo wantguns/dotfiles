a3b0586c6f7d5a211e95a2d3fbce3403eec73849
Gunwant Jain <mail@wantguns.dev>
2026-06-10 03:04:10 +0530

    alnitak,bellatrix,mintaka: migrate resolved to settings.Resolve
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- hosts/linux/bellatrix/default.nix
+++ hosts/linux/bellatrix/default.nix
@@ -67,11 +67,13 @@
   services = {
     resolved = {
       enable = true;
-      dnssec = "allow-downgrade";
-      fallbackDns = [
-        "1.1.1.1"
-        "8.8.8.8"
-      ];
+      settings.Resolve = {
+        DNSSEC = "allow-downgrade";
+        FallbackDNS = [
+          "1.1.1.1"
+          "8.8.8.8"
+        ];
+      };
     };
 
     openssh = {
