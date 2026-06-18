556462e9c7cb86a5b13ec74f7fd1e130c14ec742
Gunwant Jain <mail@wantguns.dev>
2026-06-18 19:32:02 +0530

    home/dev: package runtime with LSPs when a language is installed
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- modules/home/dev/default.nix
+++ modules/home/dev/default.nix
@@ -12,19 +12,59 @@ in
   config = lib.mkMerge [
     (lib.mkIf cfg.dev.go {
       home.packages = with pkgs; [
+        go
         gopls
+        golangci-lint
         golangci-lint-langserver
       ];
     })
-    (lib.mkIf cfg.dev.python { home.packages = [ pkgs.ty ]; })
-    (lib.mkIf cfg.dev.terraform { home.packages = [ pkgs.terraform-ls ]; })
-    (lib.mkIf cfg.dev.nodejs {
+
+    (lib.mkIf cfg.dev.rust {
       home.packages = with pkgs; [
-        typescript-language-server
-        typescript
+        rustc
+        cargo
+        rust-analyzer
       ];
     })
-    (lib.mkIf cfg.dev.lua { home.packages = [ pkgs.lua-language-server ]; })
-    (lib.mkIf cfg.dev.zig { home.packages = [ pkgs.zls ]; })
+
+    (lib.mkIf cfg.dev.python {
+      home.packages = with pkgs; [
+        uv
+        ty
+        ruff
+        python3
+      ];
+    })
+
+    (lib.mkIf cfg.dev.terraform {
+      home.packages = with pkgs; [
+        terraform
+        terraform-ls
+      ];
+    })
+
+    (lib.mkIf cfg.dev.nodejs {
+      home.packages = with pkgs; [
+        nodejs
+        pnpm
+        typescript
+        typescript-language-server
+      ];
+    })
+
+    (lib.mkIf cfg.dev.lua {
+      home.packages = with pkgs; [
+        lua
+        luarocks
+        lua-language-server
+      ];
+    })
+
+    (lib.mkIf cfg.dev.zig {
+      home.packages = with pkgs; [
+        zig
+        zls
+      ];
+    })
   ];
 }
